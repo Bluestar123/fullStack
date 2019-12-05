0ac59b1f341b196fbb58a3630203cc6cdc8257a0
wangjh <l>
2019-12-05 21:21:00 +0800

    课时注解

--- server/libs/db/src/modules/episode.model.ts
+++ server/libs/db/src/modules/episode.model.ts
@@ -1,12 +1,15 @@
 import { prop, modelOptions } from '@typegoose/typegoose';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 @modelOptions({
   schemaOptions: { timestamps: true },
 })
 export class Episode {
+  @ApiModelProperty({ description: '课时名称' })
   @prop()
   name: string;
 
+  @ApiModelProperty({ description: '课时文件' })
   @prop()
   file: string;
 }
